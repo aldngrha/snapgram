9969569f25c6dc5d2df0b3f57818ce19bb629691
Aldi Nugraha <aldi.nugrahatk@gmail.com>
2024-03-11 13:25:09 +0700

    add: Explore page

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -1,5 +1,96 @@
+import { Input } from "@/components/ui/input.tsx";
+import { useEffect, useState } from "react";
+import {
+  useGetPosts,
+  useSearchPosts,
+} from "@/lib/react-query/queriesAndMutations.ts";
+import useDebounce from "@/hooks/useDebounce.ts";
+import Loader from "@/components/shared/Loader.tsx";
+import GridPostList from "@/components/shared/GridPostList.tsx";
+import SearchResult from "@/components/shared/SearchResult.tsx";
+import { useInView } from "react-intersection-observer";
+
 const Explore = () => {
-  return <div>Explore</div>;
+  const { ref, inView } = useInView();
+  const { data: posts, fetchNextPage, hasNextPage } = useGetPosts();
+
+  const [searchValue, setSeacrhValue] = useState("");
+  const debouncedSearch = useDebounce(searchValue, 500);
+  const { data: searchedPosts, isFetching: isSearchFetching } =
+    useSearchPosts(debouncedSearch);
+
+  useEffect(() => {
+    if (inView && !searchValue) fetchNextPage();
+  }, [inView, searchValue]);
+
+  if (!posts)
+    return (
+      <div className="flex-center w-full h-full">
+        <Loader />
+      </div>
+    );
+
+  const shouldShowSeacrhPosts = searchValue !== "";
+  const shouldShowPosts =
+    !shouldShowSeacrhPosts &&
+    posts.pages.every((item) => item.documents.length === 0);
+
+  return (
+    <div className="explore-container">
+      <div className="explore-inner_container">
+        <h2 className="h3-bold md:h2-bold w-full">Search Posts</h2>
+        <div className="flex gap-1 px-4 w-full rounded-lg bg-dark-4">
+          <img
+            src="/assets/icons/search.svg"
+            alt="seacrh"
+            width={24}
+            height={24}
+          />
+          <Input
+            type="text"
+            placeholder="Seacrh"
+            className="explore-search"
+            value={searchValue}
+            onChange={(e) => setSeacrhValue(e.target.value)}
+          />
+        </div>
+      </div>
+
+      <div className="flex-between w-full max-w-5xl mt-16 mb-7">
+        <h3 className="body-bold md:h3-bold">Popular Today</h3>
+        <div className="flex-center gap-3 bg-dark-3 rounded-xl px-4 py-2 cursor-pointer">
+          <p className="small-medium md:base-medium text-light-2">All</p>
+          <img
+            src="/assets/icons/filter.svg"
+            width={20}
+            height={20}
+            alt="filter"
+          />
+        </div>
+      </div>
+
+      <div className="flex flex-wrap gap-9 w-full max-w-5xl">
+        {shouldShowSeacrhPosts ? (
+          <SearchResult
+            isSearchFetching={isSearchFetching}
+            searchedPosts={searchedPosts}
+          />
+        ) : shouldShowPosts ? (
+          <p className="text-light-4 mt-10 text-center w-full">End of posts</p>
+        ) : (
+          posts.pages.map((item, index) => (
+            <GridPostList key={`page-${index}`} posts={item.documents} />
+          ))
+        )}
+      </div>
+
+      {hasNextPage && !searchValue && (
+        <div ref={ref} className="mt-10">
+          <Loader />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Explore;
